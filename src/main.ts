64e3dda536a21564644e27b92438adf019241e21
fyyyyyyy <877385364@qq.com>
2025-05-06 22:23:33 +0800

    feat: 添加view-plus组件库

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createSSRApp } from "vue";
+import uviewPlus from 'uview-plus'
 import App from "./App.vue";
 import pinia from "./stores";
 import './styles/common.scss'
 export function createApp() {
   const app = createSSRApp(App);
+  app.use(uviewPlus);
   app.use(pinia);
   return {
     app,
--- src/pages/personalCenter/index.vue
+++ src/pages/personalCenter/index.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
+<script setup>
+import { ref } from 'vue';
 
+// 创建响应式数据  
+const text1 = ref('uview-plus UI众多组件覆盖开发过程的各个需求，组件功能丰富，多端兼容。让您快速集成，开箱即用');  
 </script>
-
 <template>
-    我的
+    <view>
+        <up-notice-bar :text="text1"></up-notice-bar>
+    </view>
 </template>
 
 <style scoped lang="scss"></style>
